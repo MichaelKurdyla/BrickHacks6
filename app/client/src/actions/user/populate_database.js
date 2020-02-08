e7ccf4a3c4fe8c121ed9485b10c5bfdff4e9a51e
Samuel White <17snb@queensu.ca>
2020-02-08 16:59:07 -0500

    populate database code

---
+++ app/client/src/actions/user/populate_database.js
@@ -0,0 +1,16 @@
+
+
+function fill(numfill){
+    i = 0
+    for(i = 0;i < numfill; i++){
+        authnum = i; 
+        if(i%2 ==0){
+            point = 050
+        }else{
+            point = 100
+        }
+
+    }
+}
+
+
