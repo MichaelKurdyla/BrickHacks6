ca1ff2af18f01163515516d1c5fa4823408dd3ee
Abby Holland <abigailholland22@gmail.com>
2020-02-09 01:06:34 -0500

    Rewards Page

--- app/client/src/components/General/Authenticated/Home.js
+++ app/client/src/components/General/Authenticated/Home.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Green from '../../../assets/green-min.jpg';
+import Cap from '../../../assets/10Dolla.png';
+import Slippers from '../../../assets/12Dolla.png';
+import Visor from '../../../assets/15Visor.png';
+import Sombrero from '../../../assets/15Sombrero.png';
+import Cooler from '../../../assets/20Dolla.png';
+import Hat from '../../../assets/28Dolla.png';
+import Chair from '../../../assets/30Dolla.png';
+
 export default function Home() {
   return (
     <div className="homepage">
@@ -13,7 +21,23 @@ export default function Home() {
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>15 Points</p>
+            </div>
+            <div className="button">
+              <button>Buy</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="custom-card">
+          <img src={Slippers} />
+          <div className="content">
+            <h3>Comfortable House Slippers</h3>
+            <p>Redeem your points for comfortable, black slippers.</p>
+          </div>
+          <div className="pricing">
+            <div className="price">
+              <p>50 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
@@ -24,8 +48,24 @@ export default function Home() {
         <div className="custom-card">
           <img src={Green} />
           <div className="content">
-            <h3>Plant a tree</h3>
+            <h3>Plant 5 trees</h3>
             <p>Redeem your points for our planet.</p>
+          </div>
+          <div className="pricing">
+            <div className="price">
+              <p>50 Points</p>
+            </div>
+            <div className="button">
+              <button>Buy</button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="custom-card">
+          <img src={Sombrero} />
+          <div className="content">
+            <h3>Fun Sombrero</h3>
+            <p>Trade in your points for a fun, corona sombrero.</p>
           </div>
           <div className="pricing">
             <div className="price">
@@ -38,14 +78,14 @@ export default function Home() {
         </div>
 
         <div className="custom-card">
-          <img src={Green} />
+          <img src={Cap} />
           <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
+            <h3>Corona Ball Cap</h3>
+            <p>Redeem your points for a nice Baseball Cap.</p>
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>50 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
@@ -54,14 +94,14 @@ export default function Home() {
         </div>
 
         <div className="custom-card">
-          <img src={Green} />
+          <img src={Visor} />
           <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
+            <h3>Summer Visor</h3>
+            <p>Trade your points in for a fun summer visor.</p>
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>45 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
@@ -70,14 +110,14 @@ export default function Home() {
         </div>
 
         <div className="custom-card">
-          <img src={Green} />
+          <img src={Hat} />
           <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
+            <h3>Funky Reef Hat</h3>
+            <p>Trade your points in for a colourful cap.</p>
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>50 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
@@ -86,14 +126,14 @@ export default function Home() {
         </div>
 
         <div className="custom-card">
-          <img src={Green} />
+          <img src={Chair} />
           <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
+            <h3>Fold-Out Chair</h3>
+            <p>Use your points to redeem a fold out chair perfect for camping.</p>
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>90 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
@@ -102,53 +142,20 @@ export default function Home() {
         </div>
 
         <div className="custom-card">
-          <img src={Green} />
+          <img src={Cooler} />
           <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
+            <h3>Drink Cooler</h3>
+            <p>Use your points to redeem a cooler.</p>
           </div>
           <div className="pricing">
             <div className="price">
-              <p>40 Points</p>
+              <p>100 Points</p>
             </div>
             <div className="button">
               <button>Buy</button>
             </div>
           </div>
         </div>
-
-        <div className="custom-card">
-          <img src={Green} />
-          <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
-          </div>
-          <div className="pricing">
-            <div className="price">
-              <p>40 Points</p>
-            </div>
-            <div className="button">
-              <button>Buy</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="custom-card">
-          <img src={Green} />
-          <div className="content">
-            <h3>Plant a tree</h3>
-            <p>Redeem your points for our planet.</p>
-          </div>
-          <div className="pricing">
-            <div className="price">
-              <p>40 Points</p>
-            </div>
-            <div className="button">
-              <button>Buy</button>
-            </div>
-          </div>
-        </div>
-
 
       </div>
 
