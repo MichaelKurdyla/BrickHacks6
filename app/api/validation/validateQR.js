611e4e50c4b0581d0070cd67b7a735225f024c00
Samuel White <17snb@queensu.ca>
2020-02-09 01:31:43 -0500

    texting stuff!

--- app/api/validation/validateQR.js
+++ app/api/validation/validateQR.js
@@ -1,5 +1,6 @@
 const QRCode = require("../../database/models/QRCode");
-
+const accountSid = 'AC5f2e5759efeae7d1653175746a1262ae';
+const authToken = '52ca7ac391ae281afcb154e6116b771a';
 exports.validate = (req, response) => {
   const code = req.body.AuthToken;
 
@@ -21,8 +22,13 @@ exports.validate = (req, response) => {
       if(res.validCode) {
         QRCode.findOneAndUpdate(filter, update)
         .then((res) => {
-          console.log(res)
-        })
+          // put my shit !!
+          const timer = setTimeout(() => {
+          console.log("would send the text");
+          reminder_text('alec','9053766343');
+          },30000);
+          return () => clearTimeout(timer);
+        });
       }
       else {
         response.send("This is an invaid key")
@@ -33,3 +39,23 @@ exports.validate = (req, response) => {
   })
 
 }
+
+function reminder_text(name, phone){
+// putting in a text verification of the account !!
+const client = require('twilio')(accountSid, authToken);
+attach = '+1'
+realNumber = attach.concat(phone)
+console.log("We are trying to text: " + realNumber)
+client.messages.create({
+    body: 'Hey ' + name + '!, just a friendly reminder to return your bottles your local liquor store !:)',
+    from: '+18509404806',
+    to: realNumber
+  })
+ .then((message) => {
+     console.log(message.sid)
+ })
+ .catch((err) => {
+     throw err
+ });
+}
+ 
